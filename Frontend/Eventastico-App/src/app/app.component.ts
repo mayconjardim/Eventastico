07e241308cdab6e7973779f7d658fa3d4fd4be05
Maycon Jardim <mayconsmill@gmail.com>
2023-04-14 16:34:29 -0300

    Instalando fontawesome e ngx-boostrap

--- Frontend/Eventastico-App/src/app/app.component.ts
+++ Frontend/Eventastico-App/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Eventastico-App';
+  max = 10;
+  rate = 7;
+  isReadonly = true;
 }
